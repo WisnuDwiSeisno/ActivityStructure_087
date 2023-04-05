eaa830ff104b9f2c810c873662c25ff9b7d475e0
WisnuDwiSeisno <127277412+WisnuDwiSeisno@users.noreply.github.com>
2023-04-05 11:21:18 +0700

    membuat structure dalam arrray selesai

--- ActivityStructure3_087/ActivityStructure3_087.cpp
+++ ActivityStructure3_087/ActivityStructure3_087.cpp
@@ -1,20 +1,45 @@
-// ActivityStructure3_087.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
+struct DetailAlamat
 {
-    std::cout << "Hello World!\n";
+	char desa[20];
+	char kota[20];
+};
+
+struct Mahasiswa
+{
+	char nim[12];
+	char nama[20];
+	DetailAlamat alamat;
+	int umur;
+};
+
+
+int main() {
+	Mahasiswa mhs[3];
+
+	for (int i = 0; i < 3; i++) {
+		cout << "Masukan NIM : ";
+		cin.getline(mhs[i].nim, 12);
+		cout << "Masukan Umur : ";
+		cin >> mhs[i].umur;
+		cin.ignore(1, '\n');
+		cout << "Masukan Nama : ";
+		cin.getline(mhs[i].nama, 20);
+		cout << "Alamat : ";
+		cout << "\n\tMasukan Desa : ";
+		cin.getline(mhs[i].alamat.desa, 20);
+		cout << "\tMasukan Kota : ";
+		cin.getline(mhs[i].alamat.kota, 20);
+	}
+
+	for (int i = 0; i < 3; i++) {
+		cout << "\n\nNIM : " << mhs[i].nim;
+		cout << "\nUmur : " << mhs[i].umur;
+		cout << "\nNama : " << mhs[i].nama;
+		cout << "\nAlamat : ";
+		cout << "\n\tDesa : " << mhs[i].alamat.desa;
+		cout << "\n\tKota : " << mhs[i].alamat.kota;
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
